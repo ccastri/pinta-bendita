e41d77546e15ce243673346cc5c9a42e40c30ac9
ccastri <camilo.castrillon@uao.edu.co>
2022-11-26 11:52:12 -0500

    warming up with the slider

---
+++ components/Slider.js
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Slider = () => {
+    return (
+
+        <div>
+            <div>
+
+                <Image
+                    width={254}
+                    height={411}
+                    alt=''
+                    src={'/Scroll.png'}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Slider
