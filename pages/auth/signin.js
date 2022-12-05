b540ebe57050ea78842b94fbf708037e6d5d9b7a
ccastri <camilo.castrillon@uao.edu.co>
2022-12-04 22:32:01 -0500

    I was too lazy cause is sunday but still I started the login component

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -7,23 +7,64 @@ function signin({ providers }) {
     return (
         <>
             {/* <Header /> */}
-            <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-20  text-center">
-                <Image src="/registerbg.png" height={780} width={600} alt="" />
+            <div className="absolute  flex flex-col items-center justify-center min-h-screen py-2 -mt-20  text-center max-h-screen" >
+
+
+                <Image
+                    className=''
+                    src="/registerbg.png" height={780} width={600} priority alt="" />
 
 
 
-                <p className="font-xs italic">{`This is not a REAL app. I'm just playin'`}</p>
-                <div className="mt-20">
+                {/* <p className="absolute font-xs z-50 italic">{`This is not a REAL app. I'm just playin'`}</p> */}
+                <form className=" absolute w-full max-w-lg ">
+                    <div className="flex flex-wrap -mx-3 mb-6">
+                        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                                Username
+                            </label>
+                            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" />
+                            <p className="text-red-500 text-xs italic">Please fill out this field.</p>
+                        </div>
+
+                    </div>
+                    <div className="flex flex-wrap -mx-3 mb-6">
+                        <div className="w-full px-3">
+                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                                Password
+                            </label>
+                            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************" />
+                        </div>
+                    </div>
+
+                    <div className=''>
+
+                        <div className="block">
+
+                            {Object.values(providers).map((provider) => (
+                                <div key={provider.name}>
+                                    <button className="  p-3 bg-blue-500 rounded-lg text-white" onClick={() => SingIntoProvider(provider.id, { callbackUrl: '/' })}>
+                                        Sign in with username
+                                    </button>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </form>
+
+
+                <div className=" top-0 block ">
 
                     {Object.values(providers).map((provider) => (
                         <div key={provider.name}>
-                            <button className=" p-3 bg-blue-500 rounded-lg text-white" onClick={() => SingIntoProvider(provider.id, { callbackUrl: '/' })}>
+                            <button className="  p-3  bg-blue-500 rounded-lg text-white" onClick={() => SingIntoProvider(provider.id, { callbackUrl: '/' })}>
                                 Sign in with {provider.name}
                             </button>
                         </div>
                     ))}
                 </div>
-            </div>
+            </div >
+
         </>
     )
 }
