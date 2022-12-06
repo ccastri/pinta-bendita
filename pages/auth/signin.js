56a2fb47ea8a2374a6e3dab3d705c0734609f53b
ccastri <camilo.castrillon@uao.edu.co>
2022-12-06 15:02:58 -0500

    This bussy days make the process slow but login is done

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -7,45 +7,50 @@ function signin({ providers }) {
     return (
         <>
             {/* <Header /> */}
-            <div className="absolute  flex flex-col items-center justify-center min-h-screen py-2 -mt-20  text-center max-h-screen" >
+            <div className="flex relative flex-col min-h-screen w-full py-2 text-center max-h-screen" >
 
 
                 <Image
-                    className=''
                     src="/registerbg.png"
                     alt=""
-                    height={780}
-                    width={600}
+                    fill
                     priority
+                // absolute={true}
                 />
 
 
+                <div className='p-5 absolute mb-10'>
+                    <h1 className='text-white text-xl bold'>Login</h1>
+
+                </div>
+
+                <div className='mt-20 absolute w-full'>
 
 
-                {/* <p className="absolute font-xs z-50 italic">{`This is not a REAL app. I'm just playin'`}</p> */}
-                <form className=" absolute w-full max-w-lg ">
-                    <div className="flex flex-wrap -mx-3 mb-6">
-                        <div className="w-80 mx-auto md:w-1/2 px-3 mb-6 md:mb-0">
-                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name">
-                                Username
-                            </label>
-                            <input className="appearance-none block w-full bg-white text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" />
-                            <p className="text-red-500 text-xs italic">Please fill out this field.</p>
+                    {/* <p className="absolute font-xs z-50 italic">{`This is not a REAL app. I'm just playin'`}</p> */}
+                    <form className=" absolute  w-full max-w-lg ">
+                        <div className="flex flex-wrap my-auto w-full -mx-3 mb-0">
+                            <div className="mx-auto md:w-1/2 px-3 mb-6 md:mb-0">
+                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name">
+                                    Username
+                                </label>
+                                <input className="appearance-none block w-full bg-white text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" />
+                                {/* <p className="text-red-500 my-auto text-xs italic">Please fill out this field.</p> */}
+                            </div>
+
+                        </div>
+                        <div className="w-full flex flex-wrap -mx-3 mb-6">
+                            <div className=" mx-auto px-3">
+                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
+                                    Password
+                                </label>
+                                <input className="appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************" />
+                            </div>
                         </div>
 
-                    </div>
-                    <div className="flex flex-wrap -mx-3 mb-6">
-                        <div className="w-80 mx-auto px-3">
-                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
-                                Password
-                            </label>
-                            <input className="appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************" />
-                        </div>
-                    </div>
+                        {/* <div className=' w-full mx-auto'> */}
 
-                    <div className=''>
-
-                        <div className=" absolute pl-10 block">
+                        <div className=" absolute mx-auto w-full items-center">
 
                             {Object.values(providers).map((provider) => (
                                 <div key={provider.name}>
@@ -55,21 +60,24 @@ function signin({ providers }) {
                                 </div>
                             ))}
                         </div>
+                        {/* </div> */}
+                    </form>
+
+
+                    <div className="absolute mx-auto -bottom-80 mt-10 w-full items-center">
+
+                        {Object.values(providers).map((provider) => (
+                            <div key={provider.name}>
+                                <button className="p-3 bg-blue-500 rounded-lg text-white" onClick={() => SingIntoProvider(provider.id, { callbackUrl: '/' })}>
+                                    Sign in with {provider.name}
+                                </button>
+                            </div>
+                        ))}
                     </div>
-                </form>
-
-
-                <div className=" block ">
-
-                    {Object.values(providers).map((provider) => (
-                        <div key={provider.name}>
-                            <button className="  p-3  bg-blue-500 rounded-lg text-white" onClick={() => SingIntoProvider(provider.id, { callbackUrl: '/' })}>
-                                Sign in with {provider.name}
-                            </button>
-                        </div>
-                    ))}
                 </div>
+
             </div >
+
 
         </>
     )
